1eeb49a456ca4410eafc59cac69cc8cc29236e8f
Johny <johny.ice10@gmail.com>
2021-01-18 15:36:11 +0600

    initial commit

---
+++ app/src/main/java/com/example/persistencebottomsheet/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.persistencebottomsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private BottomSheetBehavior bottomSheetBehavior;
+    private Button showBottomSheetDialogButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Test Case!");
+        setSupportActionBar(toolbar);
+
+
+
+        bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottomSheetLayout));
+        showBottomSheetDialogButton = (Button) findViewById(R.id.show_bottom_sheet_dialog_button);
+        showBottomSheetDialogButton.setOnClickListener(this);
+
+
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(View bottomSheet, int newState) {
+
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    //  bottomSheetHeading.setText(getString(R.string.text_collapse_me));
+                } else {
+                    //     bottomSheetHeading.setText(getString(R.string.text_expand_me));
+                }
+
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        Log.e("Bottom Sheet Behaviour", "STATE_COLLAPSED");
+                        break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        Log.e("Bottom Sheet Behaviour", "STATE_DRAGGING");
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        Log.e("Bottom Sheet Behaviour", "STATE_EXPANDED");
+                        break;
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        Log.e("Bottom Sheet Behaviour", "STATE_HIDDEN");
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        Log.e("Bottom Sheet Behaviour", "STATE_SETTLING");
+                        break;
+                }
+            }
+
+
+            @Override
+            public void onSlide(View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.show_bottom_sheet_dialog_button:
+                new CustomBottomSheet().show(getSupportFragmentManager(), "Dialog");
+                break;
+
+        }
+    }
+}
